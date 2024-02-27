4552170ab146541b708aafde997d838dc9eca614
vico <victorgomezcarrascosa@gmail.com>
2024-02-27 12:38:08 -0600

    prueba 3

--- ejrcicios P/votos/main.cpp
+++ ejrcicios P/votos/main.cpp
@@ -110,14 +110,45 @@ p_demo[i].VTOTALES=p_demo[i].V2017+p_demo[i].V2019+p_demo[i].V2021+ p_demo[i].V2
 }
 //----------------------------------------------------------------------------//----------------------------------------------------------------------------
 system("cls");
+for(int i=0;i<CANDIDATOS;i++){
+    cout<<"candidato N0 "<< i+1<<" de el partido Republicano  "<<endl;
+//----------------------------------------------------------------------------
+   cout<<"Nombre: ";
+    cin>>p_rep[i].nombre;
+    system("cls");
+//----------------------------------------------------------------------------
+//llenamos votaciones 2017 con ramdoms
+p_rep[i].V2017=busquedaAleatorios(1,MAX_VOTOS);
+//----------------------------------------------------------------------------
+//llenamos votaciones 2017 con ramdoms
+p_rep[i].V2019=busquedaAleatorios(1,MAX_VOTOS);
+//----------------------------------------------------------------------------
+//llenamos votaciones 2017 con ramdoms
+p_rep[i].V2021=busquedaAleatorios(1,MAX_VOTOS);
+//----------------------------------------------------------------------------
+//llenamos votaciones 2017 con ramdoms
+p_rep[i].V2023=busquedaAleatorios(1,MAX_VOTOS);
+//voto total P_INDEPENDIENTE
+p_rep[i].VTOTALES=p_rep[i].V2017+p_rep[i].V2019+p_rep[i].V2021+ p_rep[i].V2023;
+
+}
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+system("cls");
+
+
+
+
+
 int votoM=0;
 int votoM1=0;
+int votoM2=0;
 int votoMe=100000000;
 int votoMe1=10000000;
-string candidato_Menor,candidato_Menor1;
-string candidato_Mayor,candidato_Mayor1;
-float promedio,promedio1=0;
-int suma,suma1=0;
+int votoMe2=10000000;
+string candidato_Menor,candidato_Menor1,candidato_Menor2;
+string candidato_Mayor,candidato_Mayor1,candidato_Mayor2;
+float promedio,promedio1,promedio2=0;
+int suma,suma1,suma2=0;
 
 //----------------------------------------------------------------------------//----------------------------------------------------------------------------
 for(int i=0;i< CANDIDATOS;i++){
@@ -144,17 +175,40 @@ promedio=suma/CANDIDATOS;
 for (int i=0;i<CANDIDATOS;i++){
     suma1=suma1+p_demo[i].VTOTALES;
     //mayor votos
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
     if(p_demo[i].VTOTALES>votoM1){
 votoM1=p_demo[i].VTOTALES;
 candidato_Mayor1=p_demo[i].nombre;
 }
-
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
 if (p_demo[i].VTOTALES<votoMe1){
     votoMe1=p_demo[i].VTOTALES;
     candidato_Menor1=p_demo[i].nombre;
 }
 }
+
 promedio1=suma1/CANDIDATOS;
+
+for(int i=0;i< CANDIDATOS;i++){
+//acumulador de votos
+suma2=suma2+p_rep[i].VTOTALES;
+
+//mayor votos//
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+if(p_rep[i].VTOTALES>votoM2){
+votoM2=p_rep[i].VTOTALES;
+candidato_Mayor2=p_rep[i].nombre;
+}
+//menor votos
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+if (p_rep[i].VTOTALES<votoMe2){
+    votoMe2=p_rep[i].VTOTALES;
+    candidato_Menor2=p_rep[i].nombre;
+}
+
+}
+//promedio de votos
+promedio2=suma2/CANDIDATOS;
 //----------------------------------------------------------------------------//----------------------------------------------------------------------------
 cout<<setw(10)<<"------PARTIDO INDEPENDIENTE-----"<<endl<<endl;
 cout<<setw(9)<<"Nombre /"<<setw(9)<<"2017 /"<<setw(15)<<"2019/ "<< setw(9) << "2021 /" << setw(15) << "2023/" << setw(15) << "Votos Totales/" <<  endl<<endl;
@@ -181,6 +235,22 @@ cout<<setw(9)<<"Nombre "<<setw(9)<<"2017 "<<setw(15)<<"2019 "<< setw(9) << "2021
     cout<<"el candidato con mas votos es "<<candidato_Mayor1<<" con "<< votoM1<<" votos"<<endl<<endl;
     cout<<"el candidato con menor votos es "<<candidato_Menor1 <<" con "<< votoMe1<<" votos"<<endl<<endl;
     cout<<"el promedio de votos en el partido Democrata es "<<promedio1<<" votos"<<endl<<endl;
+
+
+
+
+
+   cout<<setw(10)<<"------PARTIDO REPUBLICANO-----"<<endl<<endl;
+cout<<setw(9)<<"Nombre /"<<setw(9)<<"2017 /"<<setw(15)<<"2019/ "<< setw(9) << "2021 /" << setw(15) << "2023/" << setw(15) << "Votos Totales/" <<  endl<<endl;
+  cout<<"------------------------------------------------------------------------------------------"<<endl;
+    //for para imprimir los datos
+    for(int i=0;i<CANDIDATOS;i++){
+     cout<<setw(9)<<p_rep[i].nombre<<setw(9)<<p_rep[i].V2017<<setw(15)<<p_rep[i].V2019<< setw(9) << p_rep[i].V2021 << setw(15) << p_rep[i].V2023 << setw(15) <<p_rep[i].VTOTALES <<  endl<<endl;
+    }
+    cout<<"el candidato con mas votos es "<<candidato_Mayor2 <<" con "<< votoM2<<" votos"<<endl<<endl;
+    cout<<"el candidato con menor votos es "<<candidato_Menor2 <<" con "<<votoMe2<<" votos"<<endl<<endl;
+    cout<<"el promedio de votos en el partido Republicano es "<<promedio2<<" votos"<<endl<<endl;
+
 
      cout << "Desea otro calculo (s/n)? ";
         cin >> opcion;
